808bfe69165a49b2b898e6ca4458ca4f1b7f32ec
Ponga Villacorta <dpongavillacorta@gmail.com>
2022-12-16 09:37:23 +0100

    vaciar deposito

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -84,6 +84,9 @@ public class MaquinaExpendedoraSimple {
     public void reducirPrecioBillete(int cantidadAReducir){
     precioBillete = precioBillete - cantidadAReducir;
 }
+    public void vaciarDepositoDinero(){ 
+    totalDineroAcumulado = 0;
+}
 }
     
 
